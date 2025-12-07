e57f386566e97b1c9367468fa8d4d4c48c82f20c
masahito <think.p.hm.0203@gmail.com>
2025-12-07 21:25:45 +0900

    オブジェクトの取得引数修正

--- script.js
+++ script.js
@@ -523,7 +523,7 @@ function renderAvailableClassesList(classes, dateString, monthKey) {
                     data-action="cancel" 
                     data-date="${dateString}" 
                     data-time="${item.startTime} - ${item.endTime}"
-                    data-reservation-id="${reservation[`${item.startTime} ${item.endTime}`].reservationId}">
+                    data-reservation-id="${reservation[`${dateString} ${item.startTime}`].reservationId}">
                 キャンセルする
             </button>
         `;
